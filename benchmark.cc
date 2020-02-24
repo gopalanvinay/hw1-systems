d447fdd60eb1261843180d63def44a30dd45e7bc
gopalanvinay <gopalanvinay@gmail.com>
2020-02-23 17:14:36 -0800

    Switched datatypes to int32_t from fast ints

--- benchmark.cc
+++ benchmark.cc
@@ -19,23 +19,24 @@ int main()
     printf("Size    Time\n");
     ofstream trash ("trash.txt", ofstream::out);
 
-    for(int_fast32_t i = pow(2,10); i <= pow(2,26); i *= 2)
+    for(int32_t i = pow(2,10); i <= pow(2,26); i *= 2)
     {
         // Create a buffer of size i and fill with random numbers
         vector<char> buffer(i);
         generate(buffer.begin(), buffer.end(), rand);
 
         // Create a vector of indices from 0 to i-1 and shuffle the indices at random
-        vector<int_fast32_t> idxs(i);
+        vector<int32_t> idxs(i);
         iota (begin(idxs), end(idxs), 0);
         random_shuffle(idxs.begin(),idxs.end());
 
+        // Save all time measurements in a vector
         vector<float> times(iters);
 
-        for(int_fast32_t j=0; j<iters;j++)
+        for(int32_t j=0; j<iters;j++)
         {
             // Get random index to read from buffer
-            int_fast32_t idx = idxs[j];
+            int32_t idx = idxs[j];
             // Time single byte read from buffer
             auto t1 = timeNow();
             c = buffer[idx];
@@ -43,9 +44,11 @@ int main()
             
             // Durations in nanoseconds
             double duration = duration_cast<nanoseconds>( t2 - t1 ).count();
+            // Store in vector
             times[j] = duration;
         }
         trash << c;
+        // Get min measurement from iters measurements
         double min_time = *min_element(times.begin(),times.end());
 
         printf("%ld  %f ns\n",i,min_time);
